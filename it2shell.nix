4c57d10ccdb9af8569c0c7035a7696c555f5f5bf
Skye J <skye@skyenet.online>
2025-10-05 16:19:19 -0400

    fix: remove old selector

--- it2shell.nix
+++ it2shell.nix
@@ -11,30 +11,29 @@ that edit $HOME/Library/Preferences/com.googlecode.iterm2.plist
 and add:
 SUEnableAutomaticChecks = 0;
 */
-let 
+let
   selector = ./iterm2_shell_integration_selector.sh;
 in
-stdenvNoCC.mkDerivation rec {
-  pname = "iterm2-shell-integration";
-  version = "0.0.1";
+  stdenvNoCC.mkDerivation rec {
+    pname = "iterm2-shell-integration";
+    version = "0.0.1";
 
-  src = fetchFromGitHub {
-    owner = "gnachman";
-    repo = "iTerm2-shell-integration";
-    rev = "2525e51";
-    hash = "sha256-ffyaGZDydQAsBnwQF4HH3T7uUg2d0xJWeLS6NKsZywI=";
-  };
+    src = fetchFromGitHub {
+      owner = "gnachman";
+      repo = "iTerm2-shell-integration";
+      rev = "2525e51";
+      hash = "sha256-ffyaGZDydQAsBnwQF4HH3T7uUg2d0xJWeLS6NKsZywI=";
+    };
 
-  dontBuild = true;
-  doCheck = false;
-  installPhase = ''
-    ls
-     mkdir -p $out/bin
-    mkdir -p $out/share/iterm2-shell-integration/
-    cp ${selector} $out/etc/profile.d/iterm2_shell_integration_selector.sh
-    cp utilities/* $out/bin
-    cp shell_integration/zsh $out/share/iterm2-shell-integration/iterm2_shell_integration.zsh
-    cp shell_integration/bash $out/share/iterm2-shell-integration/iterm2_shell_integration.bash
-    cp shell_integration/fish $out/share/iterm2-shell-integration/iterm2_shell_integration.fish
-  '';
-}
+    dontBuild = true;
+    doCheck = false;
+    installPhase = ''
+      ls
+       mkdir -p $out/bin
+      mkdir -p $out/share/iterm2-shell-integration/
+      cp utilities/* $out/bin
+      cp shell_integration/zsh $out/share/iterm2-shell-integration/iterm2_shell_integration.zsh
+      cp shell_integration/bash $out/share/iterm2-shell-integration/iterm2_shell_integration.bash
+      cp shell_integration/fish $out/share/iterm2-shell-integration/iterm2_shell_integration.fish
+    '';
+  }
