07027d7972e3e86844e9e7c16be120c9bc985900
Skye J <skye@skyenet.online>
2025-10-05 17:02:12 -0400

    debug: allow pkgs to be input

--- default.nix
+++ default.nix
@@ -1,9 +1,10 @@
-let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.05";
-  pkgs = import nixpkgs {
-    config = {};
-    overlays = [];
-  };
-in {
+{
+  pkgs ?
+    import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.05") {
+      config = {};
+      overlays = [];
+    },
+  ...
+}: {
   iterm2-shell-integration = pkgs.callPackage ./it2shell.nix {};
 }
